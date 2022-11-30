760e46d021657e17535ebfed44b58d024c5e46e7
Aaron Crowder <aaron.crowder@gmail.com>
2022-11-29 19:49:13 -0700

    werk

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This is a shitty lil script I'm working on to take the Markdown (thanks to this
 neat tool: https://github.com/timhutton/twitter-archive-parser) of my twitter
 archive and turn them into individual posts for my Hugo blog.
+
+## Disclaimer
+
+This software is provided for use at your own risk. I wrote this for myself and
+I make no guarantees whatsoever of it's stability. There may be bugs. It may
+crash your system.
--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	files, err := getFilesRecursively("./archive/tweets-md")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	contents, err := getFileContents(files)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for filePath, fileContent := range contents {
+		fmt.Printf("%s: %s\n\n", filePath, fileContent)
+	}
+}
+
+func getFilesRecursively(dir string) ([]string, error) {
+	var files []string
+
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return files, err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			additionalFiles, err := getFilesRecursively(
+				fmt.Sprintf("%s/%s", dir, entry.Name()))
+			if err != nil {
+				return files, err
+			}
+
+			files = append(files, additionalFiles...)
+		} else {
+			files = append(files, fmt.Sprintf("%s/%s", dir, entry.Name()))
+		}
+	}
+
+	return files, nil
+}
+
+func getFileContents(files []string) (map[string]string, error) {
+	log.Fatal("Not Implemented")
+	return nil, nil
 }
